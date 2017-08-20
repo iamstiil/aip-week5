cc8f88af2cc4ca5252132bcc35a25fbe75196ed6
Pavithran Gridharan <pavithran@gridharan.de>
2017-08-20 23:16:53 +1000

    feat: add icon font open-iconic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        Bootstrap is set up!
+        <div className="row">
+          <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Homize</a>
+          </nav>
+        </div>
+        <div className="row content-wrapper">
+          <div className="col-12">
+            <h1>Dashboard</h1>
+            <span className="oi oi-magnifying-glass"></span>
+          </div>
+        </div>
       </div>
     );
   }
