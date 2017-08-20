86ce0a2ded3a83038669f685308edb9f3722b47c
Pavithran Gridharan <pavithran@gridharan.de>
2017-08-20 20:17:07 +1000

    feat: setup react and bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        Bootstrap is set up!
+      </div>
+    );
+  }
+}
+
+export default App;
