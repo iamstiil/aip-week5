6edc59117bb1c8b0f8c692c0a03e8afdf5de46b9
Pavithran Gridharan <pavithran@gridharan.de>
2017-08-21 13:03:09 +1000

    refactor: add rendering based on states

--- src/App.js
+++ src/App.js
@@ -4,6 +4,91 @@ import 'bootstrap';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name: 'User 1',
+          tasks: [
+            {
+              id: 1,
+              title: 'Task 1',
+              description: 'Simple task description'
+            },
+            {
+              id: 2,
+              title: 'Task 2',
+              description: 'Simple task description'
+            },
+            {
+              id: 3,
+              title: 'Task 3',
+              description: 'Simple task description'
+            },
+          ]
+        },
+        {
+          id: 2,
+          name: 'User 2',
+          tasks: [
+            {
+              id: 4,
+              title: 'Task 1',
+              description: 'Simple task description'
+            },
+            {
+              id: 5,
+              title: 'Task 2',
+              description: 'Simple task description'
+            },
+            {
+              id: 6,
+              title: 'Task 3',
+              description: 'Simple task description'
+            },
+            {
+              id: 7,
+              title: 'Task 4',
+              description: 'Simple task description'
+            },
+          ]
+        },
+        {
+          id: 3,
+          name: 'User 3',
+          tasks: [
+            {
+              id: 8,
+              title: 'Task 1',
+              description: 'Simple task description'
+            },
+            {
+              id: 9,
+              title: 'Task 2',
+              description: 'Simple task description'
+            },
+          ]
+        },
+      ]
+    };
+  }
+  
+  _getUserLists() {
+    return this.state.users.map((user) => {
+      return (
+        <div key={user.id} className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <h3>{user.name}</h3>
+          {user.tasks.map((task) => {
+            return <Card key={task.id} title={task.title}>{task.description}</Card>
+          })}
+        </div>
+      );
+    });
+  }
+  
   render() {
     return (
       <div className="container-fluid">
@@ -23,27 +108,7 @@ class App extends Component {
           </div>
         </div>
         <div className="row">
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h3>User 1</h3>
-            <Card title="Task 1">Simple task description</Card>
-            <Card title="Task 2">Simple task description</Card>
-            <Card title="Task 3">Simple task description</Card>
-            <button className="btn btn-primary btn-block">Create Task</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h3>User 2</h3>
-            <Card title="Task 1">Simple task description</Card>
-            <Card title="Task 2">Simple task description</Card>
-            <Card title="Task 3">Simple task description</Card>
-            <Card title="Task 4">Simple task description</Card>
-            <button className="btn btn-primary btn-block">Create Task</button>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-            <h3>User 3</h3>
-            <Card title="Task 1">Simple task description</Card>
-            <Card title="Task 2">Simple task description</Card>
-            <button className="btn btn-primary btn-block">Create Task</button>
-          </div>
+          {this._getUserLists()}
         </div>
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Card.scss';
 
 class Card extends Component {
   render(){
     return (
-      <div className="card mb-3">
+      <div className="card mb-3" onClick={this.props.onClick}>
         <div className="card-body">
           <h4 className="card-title">{this.props.title}</h4>
           <p className="card-text">{this.props.children}</p>
