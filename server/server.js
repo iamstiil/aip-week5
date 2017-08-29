33a590e07e4650039093ae465ca88dac81fca84b
Ender Ismail <ender.ismail@student.uts.edu.au>
2017-08-29 09:51:49 +0200

    feat: add mongodb connection (buggy)

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient
+const co = require('co');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.use(express.static('build'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/build/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/api/users', (req, res) => {
+  let users = co(function*() {
+    const db = yield MongoClient.connect("mongodb://localhost:270172/test");
 
-app.post('/src/index.js', (req,res) => {
-  console.log(req.body);
+    let res = yield db.collection('users');
+
+    db.close();
+
+    return res;
+  }).catch(err => console.log(err));
+
+  res.send(users);
 });
 
 app.listen(8080);
