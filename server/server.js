8b53e3d408451df55dc6020010a37684a257e6b2
Pavithran Gridharan <pavithran@gridharan.de>
2017-09-06 20:23:52 +1000

    feat: add api path for getting and creating users

--- server/server.js
+++ server/server.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const co = require('co');
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/test');
 
 app.use(express.static('build'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/users', (req, res) => {
-  let users = co(function*() {
-    const db = yield MongoClient.connect("mongodb://localhost:270172/test");
+const UserSchema = new mongoose.Schema({
+  name: String,
+});
+const User = mongoose.model('User', UserSchema);
+const TaskSchema = new mongoose.Schema({
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+const Task = mongoose.model('Task', TaskSchema);
 
-    let res = yield db.collection('users');
-
-    db.close();
-
-    return res;
-  }).catch(err => console.log(err));
-
-  res.send(users);
+app.get('/api/user', (req, res) => {
+  User.find({}).exec().then((response) => {
+    res.json(response);
+  });
 });
 
-app.listen(8080);
+app.get('/api/user/:id', (req, res) => {
+  User.findById(req.param('id')).exec().then((response) => {
+    res.json(response);
+  });
+});
+
+app.post('/api/user/create', (req, res) => {
+  User.create(req.body).then((response) => {
+    res.json(response);
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
