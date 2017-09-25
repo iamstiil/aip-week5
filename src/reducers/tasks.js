4d777badec9d3cd79874d6fcf08e54c206c430f2
Pavithran Gridharan <pavithran@gridharan.de>
2017-09-25 11:32:51 +1000

    fix(task): change id prop-type to string

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -5,10 +5,10 @@ function tasks(state = [], action) {
     case CREATE_TASK: {
       const res = state.slice(0);
 
-      const id = Math.max.apply(null, state.map(task => task.id)) || -1;
+      const id = Math.max.apply(null, state.map(task => parseInt(task.id, 10))) || -1;
 
       res.push({
-        id: id + 1,
+        id: `${id + 1}`,
         title: action.task.title,
         description: action.task.description,
         userid: action.task.user,
