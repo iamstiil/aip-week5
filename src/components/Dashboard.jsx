8f4e5ca41a30aeea0d51efa723a4fee48c307c60
Pavithran Gridharan <pavithran@gridharan.de>
2017-09-25 10:51:36 +1000

    fix: show username instead of email

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -15,7 +15,7 @@ class Dashboard extends Component {
   getUserLists() {
     return this.props.users.map(user => (
       <div key={user.id} className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-        <h3>{user.name.length > 0 ? user.name : user.email}</h3>
+        <h3>{user.name.length > 0 ? user.name : user.username}</h3>
         {this.props.tasks.map((task) => {
           if (user.id !== task.userid) {
             return false;
