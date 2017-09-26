6b0de20c51abc580b8d3d526fb8ba6bc70615b93
Ender Ismail <ender.ismail@student.uts.edu.au>
2017-09-26 08:40:16 +0200

    feat: add initialization for tasks in redux

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -5,4 +5,5 @@ export const CREATE_TASK = 'CREATE_TASK';
 export const INITIALIZE = 'INITIALIZE';
 export const USER_LOGGED_IN = 'USER_LOGGED_IN';
 export const USERS_LOADED = 'USERS_LOADED';
+export const TASKS_LOADED = 'TASKS_LOADED';
 export const USER_LOGGED_OUT = 'USER_LOGGED_OUT';
