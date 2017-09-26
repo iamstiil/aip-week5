c19dbbaa1be63630a7414dfc94fc6e87744c38af
Pavithran Gridharan <pavithran@gridharan.de>
2017-09-26 16:38:55 +1000

    feat: add createTask to server

--- server/api-router/task.js
+++ server/api-router/task.js
@@ -14,9 +14,17 @@ router.use(bodyParser.json());
 router.post('/', (req, res) => {
   const { title, description, user } = req.body;
 
-  console.log(title, description, user);
+  if (!title || !user) {
+    res.status(400).json({ errors: { default: 'No input registered. Please try again.' } });
+  }
 
-  res.json({ title, description, user });
+  db.createTask(title, description, user).then((task) => {
+    if (!task) {
+      res.status(400).json({ errors: { default: 'The server had problems creating your task. Pleast try again.' } });
+    }
+
+    res.json(task);
+  });
 });
 
 module.exports = router;
