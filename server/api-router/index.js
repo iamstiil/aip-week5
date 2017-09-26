f615726c805642ebf1f63abcaf81a2570a9094ce
Pavithran Gridharan <pavithran@gridharan.de>
2017-09-26 12:53:35 +1000

    refactor: remove obsolete mongoose connection

--- server/api-router/index.js
+++ server/api-router/index.js
@@ -4,8 +4,6 @@ const userRouter = require('./user');
 
 const router = express.Router();
 
-mongoose.connect('mongodb://localhost:27017/test', { useMongoClient: true });
-
 router.use('/user', userRouter);
 
 module.exports = router;
