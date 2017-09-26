b6a183034c4b2398cae87986c7e0bc7cde7f3b2f
Ender Ismail <ender.ismail@student.uts.edu.au>
2017-09-26 08:57:08 +0200

    fix: tasks-attributes to match server response

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -20,7 +20,7 @@ class Dashboard extends Component {
       <div key={user.id} className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
         <h3>{user.name.length > 0 ? user.name : user.username}</h3>
         {this.props.tasks.map((task) => {
-          if (user.id !== task.userid) {
+          if (user.id !== task.user) {
             return false;
           }
           return (
