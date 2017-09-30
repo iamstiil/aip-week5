949bd6c09a82e4ab3776b0e85dce83bb863a885e
Pavithran Gridharan <pavithran@gridharan.de>
2017-09-30 18:41:22 +1000

    feat: show users for editing in administration

--- src/components/Administration.jsx
+++ src/components/Administration.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CustomPropTypes from '../utils/custom-prop-types';
+import Card from './Card';
+import SelectGroup from './SelectGroup';
 
 class Administration extends Component {
   constructor(props) {
@@ -10,9 +14,35 @@ class Administration extends Component {
 
   render() {
     return (
-      <div>Administration</div>
+      <div>
+        <h3>Users</h3>
+        {this.props.users.map(({ email, role, username }) => (
+          <Card subtitle={email} title={username}>
+            <div className="row">
+              <SelectGroup className="col-12 col-lg-6 col-xl-4" label="Role" value={role}>
+                <option value="Administrator">Administrator</option>
+                <option value="User">User</option>
+              </SelectGroup>
+              <div className="col-12 col-lg-6 col-xl-8">
+                <p>Actions</p>
+                <button className="btn btn-sm btn-danger">
+                  <em className="oi oi-trash" /> Delete
+                </button>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
     );
   }
 }
 
-export default Administration;
+Administration.propTypes = {
+  users: CustomPropTypes.users.isRequired,
+};
+
+export default connect(
+  state => ({
+    users: state.users,
+  }),
+)(Administration);
