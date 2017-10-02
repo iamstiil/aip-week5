c6f8373fe5983b7a1ecc3c1af93d39b76ad4c142
Pavithran Gridharan <pavithran@gridharan.de>
2017-10-02 16:35:30 +1100

    fix: replace deprecated method with new method

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
   if (req.method === 'OPTIONS') {
-    res.send(200);
+    res.sendStatus(200);
   } else {
     next();
   }
