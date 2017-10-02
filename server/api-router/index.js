41909867843f08babdd8bf07ba20a826b7e35cd9
Pavithran Gridharan <pavithran@gridharan.de>
2017-10-02 16:23:57 +1100

    feat: use tokens for fetching data from api

--- server/api-router/index.js
+++ server/api-router/index.js
@@ -2,11 +2,36 @@
  * Import dependencies
  */
 const express = require('express');
+const bearerToken = require('express-bearer-token');
+const jwt = require('jsonwebtoken');
 const taskRouter = require('./task');
 const userRouter = require('./user');
 
 // Create API router
 const router = express.Router();
+
+/**
+ * Parse token middleware
+ */
+router.use(bearerToken());
+
+/**
+ * Token validation middleware
+ */
+router.use((req, res, next) => {
+  if (req.token) {
+    jwt.verify(req.token, 'secretsecretsecretsecret', (err, decoded) => {
+      if (err) {
+        res.status(400).json({ errors: { default: 'Invalid token' } });
+      } else {
+        req.decodedToken = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ errors: { default: 'No token provided' } });
+  }
+});
 
 // include task API
 router.use('/task', taskRouter);
