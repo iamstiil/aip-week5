ef2919a8a5cbb34da0be8a3300bd3ea877f37077
Pavithran Gridharan <pavithran@gridharan.de>
2017-10-03 14:48:14 +1100

    feat: add filter/search to dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 /**
  * Import dependencies
  */
+import classnames from 'classnames';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -13,13 +14,36 @@ import './Dashboard.scss';
  */
 class Dashboard extends Component {
   /**
+   * Constructor
+   */
+  constructor(props) {
+    super(props);
+
+      filterOpen: false,
+      visibleTasks: props.tasks,
+    };
+
+    this.toggleFilter = this.toggleFilter.bind(this);
+    this.filterTasks = this.filterTasks.bind(this);
+  }
+
+  /**
+   * React lifecycle methods
+   */
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      visibleTasks: nextProps.tasks,
+    });
+  }
+
+  /**
    * Get all lists of users with tasks
    */
   getUserLists() {
     return this.props.users.map(user => (
       <div key={user.id} className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
         <h3>{user.name.length > 0 ? user.name : user.username}</h3>
-        {this.props.tasks.map((task) => {
+        {this.state.visibleTasks.map((task) => {
           if (user.id !== task.user) {
             return false;
           }
@@ -36,14 +60,52 @@ class Dashboard extends Component {
     ));
   }
 
+  /**
+   * Filter visible tasks
+   */
+  filterTasks(event) {
+    const tasks = this.props.tasks.filter((task) => {
+      if (task.title.indexOf(event.target.value) !== -1) {
+        return true;
+      }
+      if (task.description.indexOf(event.target.value) !== -1) {
+        return true;
+      }
+      return false;
+    });
+    this.setState({
+      visibleTasks: tasks,
+    });
+  }
+
+  /**
+   * View/hide filter input
+   */
+  toggleFilter() {
+    this.setState({
+      filterOpen: !this.state.filterOpen,
+    });
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-12 view-menu-wrapper">
-          <div className="view-menu float-right">
-            <button className="btn btn-outline-primary oi oi-magnifying-glass" />
-            <button className="btn btn-outline-primary oi oi-list" />
-            <button className="btn btn-outline-primary oi oi-grid-two-up" />
+          <div className="view-menu float-right form-inline">
+            <div className={classnames('filter-input', 'mr-2', { open: this.state.filterOpen })}>
+              <input className="form-control" onChange={this.filterTasks} />
+            </div>
+            <button
+              className={
+                classnames('btn', 'btn-primary', 'oi', {
+                  'oi-wrench': !this.state.filterOpen,
+                  'oi-x': this.state.filterOpen,
+                })
+              }
+              onClick={this.toggleFilter}
+            />
+            <button className="btn btn-primary oi oi-list" />
+            <button className="btn btn-primary oi oi-grid-two-up" />
           </div>
         </div>
         <div className="col-12">
