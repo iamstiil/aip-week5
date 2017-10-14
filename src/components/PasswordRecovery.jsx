98c30a53f29e3e35812b9934ef8d69fcba803eba
Ender Ismail <ender.ismail@student.uts.edu.au>
2017-10-14 07:01:57 +0200

    feat: add validation for password-recovery id

--- src/components/PasswordRecovery.jsx
+++ src/components/PasswordRecovery.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactRouterPropTypes from 'react-router-prop-types';
 import { passwordResetRequest } from '../actions';
 import InputGroup from './InputGroup';
 
@@ -10,8 +11,14 @@ import InputGroup from './InputGroup';
 class PasswordRecovery extends Component {
   constructor(props) {
     super(props);
+    let id;
+    if (props.location.search) {
+      id = props.location.search.splice(0, 1);
+    }
+    const isIdValid = new RegExp('^[a-f0-9]{24}$').test(id);
     this.state = {
       error: {},
+      id: isIdValid ? id : null,
       password: '',
       passwordConfirm: '',
       submitted: false,
@@ -33,31 +40,58 @@ class PasswordRecovery extends Component {
   render() {
     return (
       <div className="login-panel card mt-5">
-        <div className="card-block p-3">
-          <h3 className="card-title text-center mt-1">Password Recovery</h3>
-          <div className="divider mt-0" />
-          <form className="form" onSubmit={this.handleSubmit}>
-            <InputGroup
-              field="password"
-              label="New Password"
-              onChange={this.handleChange}
-              type="password"
-              value={this.state.password}
-              required
-            />
-            <InputGroup
-              field="passwordConfirm"
-              label="Confirm new Password"
-              onChange={this.handleChange}
-              type="password"
-              value={this.state.passwordConfirm}
-              required
-            />
-            <button
-              className="btn btn-primary btn-block"
-            >Submit</button>
-          </form>
-        </div>
+        {this.state.id && (
+          <div className="card-block p-3">
+            <h3 className="card-title text-center mt-1">Password Recovery</h3>
+            <div className="divider mt-0" />
+            <form className="form" onSubmit={this.handleSubmit}>
+              <input type="text" name="id" value={this.state.id} hidden />
+              <InputGroup
+                field="password"
+                label="New Password"
+                onChange={this.handleChange}
+                type="password"
+                value={this.state.password}
+                required
+              />
+              <InputGroup
+                field="passwordConfirm"
+                label="Confirm new Password"
+                onChange={this.handleChange}
+                type="password"
+                value={this.state.passwordConfirm}
+                required
+              />
+              <button
+                className="btn btn-primary btn-block"
+              >Submit</button>
+            </form>
+          </div>
+        )}
+        {!this.state.id && (
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="error-template text-center">
+                  <h1>Oops!</h1>
+                  <h2>404 Not Found</h2>
+                  <div className="error-details">
+                    <p>Sorry, an error has occured, Requested page not found!</p>
+                  </div>
+                  <div className="error-actions">
+                    <a href="/" className="btn btn-primary btn-lg">
+                      <span className="glyphicon glyphicon-home" />
+                      Take Me Home
+                    </a>
+                    <a href="http://www.jquery2dotnet.com" className="btn btn-default btn-lg">
+                      <span className="glyphicon glyphicon-envelope" /> Contact Support
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
@@ -68,6 +102,7 @@ class PasswordRecovery extends Component {
  */
 PasswordRecovery.propTypes = {
   passwordResetRequest: PropTypes.func.isRequired,
+  location: ReactRouterPropTypes.location.isRequired,
 };
 
 export default connect(
