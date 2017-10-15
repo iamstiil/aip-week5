8ef2c28b6819f4abea23c56e9b1e12c21151ab2e
Pavithran Gridharan <pavithran@gridharan.de>
2017-10-15 19:05:23 +1100

    feat: allow cors on production

--- server/server.js
+++ server/server.js
@@ -10,18 +10,16 @@ const constants = require('../src/constants');
 const app = express();
 
 // Add middleware to allow CORS
-if (process.env.NODE_ENV === 'development') {
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', `http://localhost:3000`);
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
-}
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', `http://localhost:3000, http://${constants.appUrl}`);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // Add authentication router
 app.use('/auth', authRouter);
